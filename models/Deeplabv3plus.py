903eff468046a488de4001ab5ea27173eefed253
Sunny <42291810+qiuluyi@users.noreply.github.com>
2023-04-21 22:36:35 +0800

    Create Deeplabv3plus.py

---
+++ models/Deeplabv3plus.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import timm
+
+class _ConvBNReLU(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0,
+                 dilation=1, groups=1, relu6=False, norm_layer=nn.BatchNorm2d, **kwargs):
+        super(_ConvBNReLU, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias=False)
+        self.bn = norm_layer(out_channels)
+        self.relu = nn.ReLU6(True) if relu6 else nn.ReLU(True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+class _ASPP(nn.Module):
+    def __init__(self, in_channels, atrous_rates, norm_layer, norm_kwargs, **kwargs):
+        super(_ASPP, self).__init__()
+        out_channels = 256
+        self.b0 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            norm_layer(out_channels, **({} if norm_kwargs is None else norm_kwargs)),
+            nn.ReLU(True)
+        )
+
+        rate1, rate2, rate3 = tuple(atrous_rates)
+        self.b1 = _ASPPConv(in_channels, out_channels, rate1, norm_layer, norm_kwargs)
+        self.b2 = _ASPPConv(in_channels, out_channels, rate2, norm_layer, norm_kwargs)
+        self.b3 = _ASPPConv(in_channels, out_channels, rate3, norm_layer, norm_kwargs)
+        self.b4 = _AsppPooling(in_channels, out_channels, norm_layer=norm_layer, norm_kwargs=norm_kwargs)
+        self.project = nn.Sequential(
+            nn.Conv2d(5 * out_channels, out_channels, 1, bias=False),
+            norm_layer(out_channels, **({} if norm_kwargs is None else norm_kwargs)),
+            nn.ReLU(True),
+            nn.Dropout(0.5)
+        )
+
+    def forward(self, x):
+        feat1 = self.b0(x)
+        feat2 = self.b1(x)
+        feat3 = self.b2(x)
+        feat4 = self.b3(x)
+        feat5 = self.b4(x)
+        x = torch.cat((feat1, feat2, feat3, feat4, feat5), dim=1)
+        x = self.project(x)
+        return x
+
+def get_model(model_name, pretrained=True):
+    model = timm.create_model(model_name, pretrained=pretrained, features_only=True)
+    return model
+
+class _FCNHead(nn.Module):
+    def __init__(self, in_channels, channels, norm_layer=nn.BatchNorm2d):
+        super(_FCNHead, self).__init__()
+        inter_channels = in_channels // 4
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+            norm_layer(inter_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1),
+            nn.Conv2d(inter_channels, channels, 1)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class _ASPPConv(nn.Module):
+    def __init__(self, in_channels, out_channels, atrous_rate, norm_layer):
+        super(_ASPPConv, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=atrous_rate, dilation=atrous_rate, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class _AsppPooling(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer):
+        super(_AsppPooling, self).__init__()
+        self.gap = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        size = x.size()[2:]
+        pool = self.gap(x)
+        out = F.interpolate(pool, size, mode='bilinear', align_corners=True)
+        return out
+
+class _ASPP(nn.Module):
+    def __init__(self, in_channels, atrous_rates, norm_layer):
+        super(_ASPP, self).__init__()
+        out_channels = 256
+        self.b0 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True)
+        )
+        rate1, rate2, rate3 = tuple(atrous_rates)
+        self.b1 = _ASPPConv(in_channels, out_channels, rate1, norm_layer)
+        self.b2 = _ASPPConv(in_channels, out_channels, rate2, norm_layer)
+        self.b3 = _ASPPConv(in_channels, out_channels, rate3, norm_layer)
+        self.b4 = _AsppPooling(in_channels, out_channels, norm_layer=norm_layer)
+        self.project = nn.Sequential(
+            nn.Conv2d(5 * out_channels, out_channels, 1, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True),
+            nn.Dropout(0.5)
+        )
+
+    def forward(self, x):
+        feat1 = self.b0(x)
+        feat2 = self.b1(x)
+        feat3 = self.b2(x)
+        feat4 = self.b3(x)
+        feat5 = self.b4(x)
+        x = torch.cat((feat1, feat2, feat3, feat4, feat5), dim=1)
+        x = self.project(x)
+        return x
+
+
+class DeepLabV3Plus(nn.Module):
+
+    def __init__(self, nclass, **kwargs):
+        super(DeepLabV3Plus, self).__init__()
+        aux = True
+        dilated = True
+        self.aux = aux
+        self.nclass = nclass
+        self.pretrained = get_model('efficientnet_b2')
+        self.head = _DeepLabHead(nclass, c1_channels=24, **kwargs)
+        if aux:
+            self.auxlayer = _FCNHead(728, nclass)
+
+    def base_forward(self, x):
+        features = self.pretrained(x)
+        return features[1], features[4]
+
+    def forward(self, x):
+        size = x.size()[2:]
+        c1, c4 = self.base_forward(x)
+        x = self.head(c4, c1)
+        x = F.interpolate(x, size, mode='bilinear', align_corners=True)
+        return x
+
+
+class _DeepLabHead(nn.Module):
+    def __init__(self, nclass, c1_channels=128, norm_layer=nn.BatchNorm2d):
+        super(_DeepLabHead, self).__init__()
+        self.aspp = _ASPP(352, [12, 24, 36], norm_layer)
+        self.c1_block = _ConvBNReLU(c1_channels, 48, 3, padding=1, norm_layer=norm_layer)
+        self.block = nn.Sequential(
+            _ConvBNReLU(304, 256, 3, padding=1, norm_layer=norm_layer),
+            nn.Dropout(0.5),
+            _ConvBNReLU(256, 256, 3, padding=1, norm_layer=norm_layer),
+            nn.Dropout(0.1),
+            nn.Conv2d(256, nclass, 1))
+
+    def forward(self, x, c1):
+        size = c1.size()[2:]
+        c1 = self.c1_block(c1)
+        x = self.aspp(x)
+        x = F.interpolate(x, size, mode='bilinear', align_corners=True)
+        return self.block(torch.cat([x, c1], dim=1))
+
+if __name__ == '__main__':
+    net = DeepLabV3Plus(nclass=6)
+    x = torch.randn(2, 3, 256, 256)
+    y = net(x)
